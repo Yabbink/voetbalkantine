480fa346ef17a40ca4ca11216bf879a803b9dc7e
Yannick <yannick.huet@student.graafschapcollege.nl>
2024-04-08 19:11:27 +0200

    second commit

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 // Database variables
-define('DB_HOST', "localhost");
-define('DB_NAME', "voetbalkantine");
-define('DB_USER', "root");
-define('DB_PASSWORD', "");
+define('DB_HOST', "localhost:3306");
+define('DB_NAME', "net24yhuet_voetbalkantine");
+define('DB_USER', "net24yhuet");
+define('DB_PASSWORD', "}p@hCauYq{M0");
 
 ?>
